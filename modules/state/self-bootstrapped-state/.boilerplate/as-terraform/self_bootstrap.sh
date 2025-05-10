bd2d7161deacf9b9f45b917cd7bfa7cde125d8c8
je-sidestuff <gerald.halimak@gmail.com>
2025-05-10 19:22:20 +0000

    feat: Support scaffold fully for self-bootstrapped-state.

---
+++ modules/state/self-bootstrapped-state/.boilerplate/as-terraform/self_bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script executes the multi-phase process of self-bootstrapping.
+# It first applies the module with the assumption that it has not yet been applied, and then runs the migration command.
+# It then plans again to verify no changes.
+
+terraform init -input=false
+terraform apply --auto-approve
+terraform init -input=false -migrate-state -force-copy
+terraform plan
