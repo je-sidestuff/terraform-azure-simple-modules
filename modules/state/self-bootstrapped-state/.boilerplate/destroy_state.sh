bb449d32ac0b9ae532ba1b3820b6755158388e3a
je-sidestuff <gerald.halimak@gmail.com>
2025-05-24 12:33:47 +0000

    feat: Improve scaffolding support for bootstrapping modules.

--- modules/state/self-bootstrapped-state/.boilerplate/destroy_state.sh
+++ modules/state/self-bootstrapped-state/.boilerplate/destroy_state.sh
@@ -4,7 +4,11 @@
 # It first applies the module with local state, and then runs the migration command.
 # It then destroys the module once the state has been brought local.
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd "${SCRIPT_DIR}/.."
 export TF_VAR_enable_remote=false
 terraform apply --auto-approve
 terraform init -input=false -migrate-state -force-copy
 terraform destroy --auto-approve
+cd -
---
+++ modules/state/self-bootstrapped-state/.boilerplate/self_bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script executes the multi-phase process of self-bootstrapping.
+# It first applies the module with the assumption that it has not yet been applied, and then runs the migration command.
+# It then plans again to verify no changes.
+
+# We also delete the backend file if it already exists - this is necessary
+# for the terragrunt-based process where we want the state to be scaffolded in a remote configuration in some cases.
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+if [ -f "${SCRIPT_DIR}/backend.tf" ]; then
+    echo "Removing backend.tf to prepare for self-bootstrapping in terragrunt configuration."
+    rm "${SCRIPT_DIR}/backend.tf"
+fi
+
+cd "${SCRIPT_DIR}/.."
+terraform init -input=false
+terraform apply --auto-approve
+terraform init -input=false -migrate-state -force-copy
+terraform plan
+cd -
