84225049426c410e38f63cf6570f93898a50d50d
je-sidestuff <gerald.halimak@gmail.com>
2025-07-06 12:25:23 +0000

    feat: Improve scaffolding support for bootstrapping modules.

--- modules/state/self-bootstrapped-state/outputs.tf
+++ modules/state/self-bootstrapped-state/outputs.tf
@@ -1,6 +1,21 @@
+output "container_name" {
+  description = "The name of the state's storage container."
+  value = azurerm_storage_container.state_container.name
+}
+
 output "migrate_state_command" {
-    description = "The command to migrate state to the new storage account."
+  description = "The command to migrate state to the new storage account."
   value = "terraform init -input=false -migrate-state -force-copy"
+}
+
+output "resource_group_name" {
+  description = "The name of the resource group where the state storage will be deployed."
+  value = azurerm_resource_group.state_resource_group.name
+}
+
+output "storage_account_name" {
+  description = "The name of the storage account where the state storage will be deployed."
+  value = azurerm_storage_account.state_storage_account.name
 }
 
 output "terragrunt_backend_generator" {
