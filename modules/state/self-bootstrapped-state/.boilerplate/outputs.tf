30594b329e5eb3da9b9e74971dc6a829aa26b1d1
je-sidestuff <gerald.halimak@gmail.com>
2025-07-17 20:28:57 +0000

    feat: Improve scaffolding support for bootstrapping modules.

--- modules/state/self-bootstrapped-state/.boilerplate/outputs.tf
+++ modules/state/self-bootstrapped-state/.boilerplate/outputs.tf
@@ -1,5 +1,17 @@
+output "container_name" {
+  value = module.state.container_name
+}
+
 output "migrate_state_command" {
   value = module.state.migrate_state_command
+}
+
+output "resource_group_name" {
+  value = module.state.resource_group_name
+}
+
+output "storage_account_name" {
+  value = module.state.storage_account_name
 }
 
 output "terragrunt_backend_generator" {
